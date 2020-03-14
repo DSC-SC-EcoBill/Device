2d202908c4b01a4ea08fecc358fff2ab3e9176c2
ksanbal <dev.ksanbal@gmail.com>
2020-03-14 18:39:27 +0900

    카페 메뉴들을 추가했어

---
+++ Device/Items.py
@@ -0,0 +1,40 @@
+class Item:
+    def __init__(self, name, price):
+        self.name = name
+        self.price = price
+
+
+# Coffee & Espresso
+cof_cappuccino = Item('Cold Foam Cappuccino', 6100)
+cof_caramel = Item('Caramel Macchiato', 5600)
+cof_mocha = Item('Caffe Mocha', 5100)
+cof_latte = Item('Caffe Latte', 4600)
+cof_decaf = Item('Decaf Caffe Americano', 4400)
+cof_Americano = Item('Caffe Americano', 4100)
+cof_brewed = Item('Brewed Coffee', 3800)
+
+# Teavana
+tea_vanilla = Item('Vanilla Black Tea Latee',5900)
+tea_green = Item('Green Tea Latte', 5900)
+tea_lemon = Item('Lemon Mint Tea', 5600)
+tea_lime = Item('Lime Passion Tea', 5600)
+tea_grapefruit = Item('Grapefruit Honey Black tea', 5300)
+tea_chai = Item('Chai Tea Latte', 5100)
+tea_earl = Item('Earl Grey Tea Latte', 5100)
+
+# Fizzio
+fiz_pink = Item('Pink Grapefruite', 6300)
+fiz_lime = Item('Cool Lime', 5900)
+fiz_black = Item('Black Tea Lemonade', 5400)
+
+# Chocolate
+cho_signature = Item('Signature Chocolate', 5300)
+
+# Frappuccino
+fra_java = Item('Java Chip', 6100)
+fra_white = Item('White Chocolate Mocha', 5700)
+fra_mocha = Item('Mocha', 5600)
+fra_green = Item('Green Tea Cream', 6300)
+fra_chocolate = Item('Chocolate Cream Chip', 5700)
+fra_strawberries = Item('Strawberries Cream', 5600)
+fra_avocado = Item('Avocado', 6900)
